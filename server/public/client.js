ea7b00171f843bc9af76f10fb7868bdbdbef7daa
Natalie McMann <nmcmann110@gmail.com>
2022-12-09 17:28:55 -0600

    got addition and clear inputs buttons and functions working

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,31 @@
+//global variables
+let operation; //store userInput operator here
+
+//jquery ready
 $(document).ready(onReady);
 
+//code to run when DOM is initially rendered
 function onReady(){
     console.log('jQuery is ready');
+    $('#addition').on('click', handleAddition);
+    $('#equal').on('click', handleDataSubmit);
+    $('#clearInputs').on('click', handleClearInputs);
+}
+
+//assigns the variable operation to addition operator value as a string
+function handleAddition(){
+    console.log('addition!');
+    let operation = '+';
+    console.log(operation);
+}
+
+function handleDataSubmit(){
+    console.log('submit data!');
+}
+
+//clears input fields upon button click
+function handleClearInputs(){
+    // console.log('clear!');
+    $('#numberOne').val('');
+    $('#numberTwo').val('');
 }
